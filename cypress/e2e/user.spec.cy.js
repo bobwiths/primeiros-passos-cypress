17a6a1d5994101f84e6f200c904879cf7b298094
bobwiths <marcelosachett@gmail.com>
2025-04-25 15:23:45 -0300

    add random values on user specs chancejs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,51 +4,25 @@ import DashboardPage from '../pages/dashboardPage.js'
 import MenuPage from '../pages/menuPage.js'
 import MyInfoPage from '../pages/myInfoPage.js'
 
+const Chance = require('chance')
+
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
 const myInfoPage = new MyInfoPage()
+const chance = new Chance()
+
 
 describe('Orange HRM Tests', () => {
 
-  // const selectorsList = {
-    
-    
-  //   firstNameField: "[name='firstName']",
-  //   middleNameField: "[name='middleName']",
-  //   lastNameField: "[name='lastName']",
-  //   genericField: ".oxd-input--active",
-  //   dateField: "[placeholder='yyyy-dd-mm']",
-  //   dateCloseButton: ".--close",
-  //   submitButton: "[type='submit']",
-  //   genericCombobox: ".oxd-select-text--arrow",
-  //   fItemDCombobox: ".oxd-select-dropdown > :nth-child(1)",
-  //   sItemDCombobox: ".oxd-select-dropdown > :nth-child(2)",
-  //   tItemDCombobox: ".oxd-select-dropdown > :nth-child(3)",
-    
-  },
-
-  it.only('User Info Update - Success', () => {
+  it('User Info Update - Success', () => {
     loginPage.accessLoginPage()
     loginPage.loginWithAnyUser(userData.userSucess.username, userData.userSucess.password)
     dashboardPage.checkDashboardPage()
     menuPage.accessMyInfo()
-    myInfoPage.userInfoUpdateSuccess()
-   
-  }),
-  it('Login - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSucess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSucess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-  }),
-  it('Login - Fail', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userFail.username)
-    cy.get(selectorsList.passwordField).type(userData.userFail.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.get(selectorsList.wrongCredentialAlert)
-  }),
-)
+    myInfoPage.fillPersonalDetails(chance.first(), chance.string(), chance.last())
+    myInfoPage.fillEmployeeDetails(chance.natural({min: 6, max: 10}), chance.natural({min: 6, max: 29}),chance.natural({min: 6, max: 29}), '2028-23-08')
+    myInfoPage.fillStatus('1998-03-02')
+    myInfoPage.saveForm()   
+ })
+})
--- cypress/pages/myInfoPage.js
+++ cypress/pages/myInfoPage.js
@@ -16,21 +16,27 @@ class MyInfoPage {
         return selectors
     }
 
-    userInfoUpdateSuccess() {
-        cy.get(this.selectorsList().firstNameField).clear().type('John')
-        cy.get(this.selectorsList().middleNameField).clear().type('Doe')
-        cy.get(this.selectorsList().lastNameField).clear().type('Junior')
-        cy.get(this.selectorsList().genericField).eq(3).clear().type('1213')
-        cy.get(this.selectorsList().genericField).eq(4).clear().type('1415')
-        cy.get(this.selectorsList().genericField).eq(5).clear().type('00000')
-        cy.get(this.selectorsList().dateField).eq(0).clear().type('2025-23-12')
+    fillPersonalDetails(firstName, middleName, lastName) {
+        cy.get(this.selectorsList().firstNameField).clear().type(firstName)
+        cy.get(this.selectorsList().middleNameField).clear().type(middleName)
+        cy.get(this.selectorsList().lastNameField).clear().type(lastName)
+    }
+    fillEmployeeDetails(employeeId, otherId, driverLicenseDate, licenseExpiryDate){
+        cy.get(this.selectorsList().genericField).eq(3).clear().type(employeeId)
+        cy.get(this.selectorsList().genericField).eq(4).clear().type(otherId)
+        cy.get(this.selectorsList().genericField).eq(5).clear().type(driverLicenseDate)
+        cy.get(this.selectorsList().dateField).eq(0).clear().type(licenseExpiryDate)
         cy.get(this.selectorsList().dateCloseButton).click()
+    }
+    fillStatus(birthDate) {
         cy.get(this.selectorsList().genericCombobox).eq(0).click({force: true})
         cy.get(this.selectorsList().tItemDCombobox).click()
         cy.get(this.selectorsList().genericCombobox).eq(1).click({force: true})
         cy.get(this.selectorsList().sItemDCombobox).click()
-        cy.get(this.selectorsList().dateField).eq(1).clear().type('1998-03-02')
-        cy.get(this.selectorsList().dateCloseButton).click()
+        cy.get(this.selectorsList().dateField).eq(1).clear().type(birthDate)
+        cy.get(this.selectorsList().dateCloseButton).click()    
+    }
+    saveForm(){
         cy.get(this.selectorsList().submitButton).eq(0).click({force: true})
         cy.get('body').should('contain', 'Successfully Updated')
         cy.get('.oxd-toast-close')
